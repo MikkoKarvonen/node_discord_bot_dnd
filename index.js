026d51366561f861c0e541860af19078585e27fb
Mikko <mkarvonen34@gmail.com>
2021-04-06 10:16:14 +0300

    Add ability to add data for event and add season and episode

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ require("dotenv").config();
 const client = new Discord.Client();
 
 const prefix = "!";
+let season = 1;
+let episode = 1;
 
 client.on("message", function (message) {
   if (!message.content.startsWith(prefix)) return;
@@ -13,7 +15,24 @@ client.on("message", function (message) {
   const command = args.shift();
 
   if (command === "event") {
-    message.reply(`New event.`);
+    const date = new Date(args.shift());
+    const name = args.join(" ");
+
+    const localeDate = date.toLocaleString("fi-FI");
+    message.reply(`
+    🎲 DnD S${season <= 9 ? "0" + season : season}E${
+      episode <= 9 ? "0" + episode : episode
+    }${name ? `: ${name}` : ""} 🎲
+    🕒 ${localeDate} 
+    `);
+  }
+
+  if (command === "setEpisode") {
+    episode = args.length > 0 ? args[0] : episode + 1;
+  }
+
+  if (command === "setSeason") {
+    season = args.length > 0 ? args[0] : season + 1;
   }
 });
 
